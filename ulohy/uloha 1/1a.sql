18edbaa99b4b428cdd468e06d3c4d7a0cd231b29
roseuniverse7411 <lenkafilipova741@gmail.com>
2026-09-22 12:00:15 +0200

    update

--- ulohy/uloha 1/1a.sql
+++ ulohy/uloha 1/1a.sql
@@ -20,12 +20,12 @@ CREATE TABLE orders (
     order_id VARCHAR(20) PRIMARY KEY,
     customer_id VARCHAR(20),
     product_id VARCHAR(20),
-    order_date DATE
-    ship_date DATE
-    sales DECIMAL(10,2)
-    quantity INT
-    discount DECIMAL(10,2)
-    profit DECIMAL(10,2)
+    order_date DATE,
+    ship_date DATE,
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(10,2),
+    profit DECIMAL(10,2),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
     Foreign Key (product_id) REFERENCES products(product_id)
 
